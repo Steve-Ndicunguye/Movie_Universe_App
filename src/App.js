6bb1b317d6a7b2772164fbcc577fbf7cd9171a3a
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2023-01-03 11:08:06 -0800

    movie universe app setup

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     const [searchTerm, setSearchTerm] = useState("")
 
     useEffect(() => {
-        searchMovies("superman")
+        searchMovies("avengers")
     }, [])
 
     const searchMovies = async (title) => {
